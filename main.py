e9eb711810df09795524c41a5fc2ef849b6d6ecf
wojtekwew <121403230+wojtekwew@users.noreply.github.com>
2022-12-27 14:18:30 +0100

    1 wersja

--- main.py
+++ main.py
@@ -1,29 +1,76 @@
-let xmeteor = 0
-let gracz = game.createSprite(2, 3)
-let speed = 900
-let meteor = game.createSprite(randint(1, 3), 0)
-basic.forever(function () {
-    if (gracz.isTouchingEdge()) {
-        gracz.delete()
-        gracz = game.createSprite(meteor, 3)
-    }
-})
-basic.forever(function () {
-    basic.pause(speed)
-    meteor.change(LedSpriteProperty.Y, 1)
-    if (meteor.isTouchingEdge()) {
-        basic.pause(200)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-        basic.clearScreen()
+def on_button_pressed_a():
+    gracz.move(-1)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    gracz.move(1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+xmeteor = 0
+meteorb: game.LedSprite = None
+gracz: game.LedSprite = None
+czas = 100
+gracz = game.create_sprite(2, 3)
+meteor = game.create_sprite(randint(1, 3), 0)
+
+def on_forever():
+    if meteor.is_touching(gracz):
         meteor.delete()
-        speed += -25
-        meteor = game.createSprite(randint(1, 3), 0)
-        xmeteor = randint(1, 3)
-    }
-})
+        game.game_over()
+basic.forever(on_forever)
+
+def on_forever2():
+    global meteorb, xmeteor
+    for index in range(5):
+        basic.pause(300)
+        meteorb.change(LedSpriteProperty.Y, 1)
+    if meteorb.is_touching_edge():
+        music.play_sound_effect(music.create_sound_effect(WaveShape.SAWTOOTH,
+                400,
+                600,
+                255,
+                0,
+                100,
+                SoundExpressionEffect.WARBLE,
+                InterpolationCurve.LINEAR),
+            SoundExpressionPlayMode.IN_BACKGROUND)
+        game.add_score(1)
+        basic.pause(200)
+        meteorb.delete()
+        meteorb = game.create_sprite(randint(0, 4), 0)
+        xmeteor = randint(0, 4)
+basic.forever(on_forever2)
+
+def on_forever3():
+    global meteorb
+    if czas == 0:
+        meteorb = game.create_sprite(randint(1, 3), 0)
+basic.forever(on_forever3)
+
+def on_forever4():
+    if gracz.is_touching(meteor):
+        game.game_over()
+basic.forever(on_forever4)
+
+def on_forever5():
+    global czas, meteor, xmeteor
+    for index2 in range(5):
+        basic.pause(300)
+        meteor.change(LedSpriteProperty.Y, 1)
+    if meteor.is_touching_edge():
+        music.play_sound_effect(music.create_sound_effect(WaveShape.SAWTOOTH,
+                400,
+                600,
+                255,
+                0,
+                100,
+                SoundExpressionEffect.WARBLE,
+                InterpolationCurve.LINEAR),
+            SoundExpressionPlayMode.IN_BACKGROUND)
+        czas += -5
+        game.add_score(1)
+        basic.pause(200)
+        meteor.delete()
+        meteor = game.create_sprite(randint(0, 4), 0)
+        xmeteor = randint(0, 4)
+basic.forever(on_forever5)
--- main.ts
+++ main.ts
@@ -5,31 +5,63 @@ input.onButtonPressed(Button.B, function () {
     gracz.move(1)
 })
 let xmeteor = 0
+let meteorb: game.LedSprite = null
 let gracz: game.LedSprite = null
+let czas = 100
 gracz = game.createSprite(2, 3)
-let speed = 900
 let meteor = game.createSprite(randint(1, 3), 0)
+let jestmeteorb = false
 basic.forever(function () {
-    if (gracz.isTouchingEdge()) {
-        gracz.delete()
-        gracz = game.createSprite(xmeteor, 3)
+    if (jestmeteorb == true) {
+        if (gracz.isTouching(meteorb)) {
+            meteor.delete()
+            meteorb.delete()
+            game.gameOver()
+        }
     }
 })
 basic.forever(function () {
     if (gracz.isTouching(meteor)) {
+        meteor.delete()
+        if (jestmeteorb == true) {
+            meteorb.delete()
+        }
         game.gameOver()
     }
 })
 basic.forever(function () {
-    basic.pause(speed)
-    meteor.change(LedSpriteProperty.Y, 1)
+    if (czas > -1 && czas < 6) {
+        jestmeteorb = true
+        meteorb = game.createSprite(randint(1, 3), 0)
+        for (let index = 0; index < 50; index++) {
+            for (let index = 0; index < 5; index++) {
+                basic.pause(300)
+                meteorb.change(LedSpriteProperty.Y, 1)
+            }
+            if (meteorb.isTouchingEdge()) {
+                music.playSoundEffect(music.createSoundEffect(WaveShape.Sawtooth, 400, 600, 255, 0, 100, SoundExpressionEffect.Warble, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+                game.addScore(1)
+                basic.pause(200)
+                meteorb.delete()
+                meteorb = game.createSprite(randint(0, 4), 0)
+                xmeteor = randint(0, 4)
+            }
+        }
+        game.gameOver()
+    }
+})
+basic.forever(function () {
+    for (let index = 0; index < 5; index++) {
+        basic.pause(300)
+        meteor.change(LedSpriteProperty.Y, 1)
+    }
     if (meteor.isTouchingEdge()) {
         music.playSoundEffect(music.createSoundEffect(WaveShape.Sawtooth, 400, 600, 255, 0, 100, SoundExpressionEffect.Warble, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+        czas += -5
         game.addScore(1)
         basic.pause(200)
         meteor.delete()
-        speed += -25
-        meteor = game.createSprite(randint(1, 3), 0)
-        xmeteor = randint(1, 3)
+        meteor = game.createSprite(randint(0, 4), 0)
+        xmeteor = randint(0, 4)
     }
 })
